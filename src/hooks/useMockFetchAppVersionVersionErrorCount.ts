af9a65e3d3d9a14b65b975de30b14c063572f227
jpundev <jordispun@gmail.com>
2025-04-14 03:28:57 -0700

    crash.count

--- src/hooks/useMockFetchAppVersionVersionErrorCount.ts
+++ src/hooks/useMockFetchAppVersionVersionErrorCount.ts
@@ -8,38 +8,39 @@ type MockFetchAppVersionCrashChartParams = {
   version?: string;
   platform?: PlatformTypes;
 };
-
+// prettier-ignore
 const mockResponse = {
-  end: "2025-03-17T12:30:00Z",
-  start: "2025-03-16T12:30:00Z",
-  step: 600,
-  data: [
+  "end": "2025-03-17T12:30:00Z",
+  "start": "2025-03-16T12:30:00Z",
+  "step": 600,
+  "data": [
     {
-      app_version: "25.11.0.1",
-      platform: PlatformTypes.IOS,
-      crash_count: [2, 1, 4, 0],
-      timestamp: [1742143800, 1742161800, 1742180600, 1742199000],
+      "app_version": "25.11.0.1",
+      "platform": "IOS",
+      "crash.count": [2, 1, 4, 0],
+      "timestamp": [1742143800, 1742161800, 1742180600, 1742199000]
     },
     {
-      app_version: "25.11.0.2",
-      platform: PlatformTypes.IOS,
-      crash_count: [0, 3, 2, 5],
-      timestamp: [1742143800, 1742161800, 1742180600, 1742199000],
+      "app_version": "25.11.0.2",
+      "platform": "IOS",
+      "crash.count": [0, 3, 2, 5],
+      "timestamp": [1742143800, 1742161800, 1742180600, 1742199000]
     },
     {
-      app_version: "25.11.0.3",
-      platform: PlatformTypes.ANDROID,
-      crash_count: [1, 2, 3, 1],
-      timestamp: [1742143800, 1742161800, 1742180600, 1742199000],
+      "app_version": "25.11.0.3",
+      "platform": "ANDROID",
+      "crash.count": [1, 2, 3, 1],
+      "timestamp": [1742143800, 1742161800, 1742180600, 1742199000]
     },
     {
-      app_version: "25.12.0.1",
-      platform: PlatformTypes.ANDROID,
-      crash_count: [3, 1, 0, 2],
-      timestamp: [1742143800, 1742161800, 1742180600, 1742199000],
-    },
-  ],
-};
+      "app_version": "25.12.0.1",
+      "platform": "ANDROID",
+      "crash.count": [3, 1, 0, 2],
+      "timestamp": [1742143800, 1742161800, 1742180600, 1742199000]
+    }
+  ]
+}
+
 export const useMockFetchAppVersionVersionErrorCount = ({
   platform,
 }: MockFetchAppVersionCrashChartParams) => {
@@ -60,7 +61,9 @@ export const useMockFetchAppVersionVersionErrorCount = ({
 
       const flattened: AppVersionCrashChartResponseDataType[] = [];
 
-      platformFiltered.forEach(({ app_version, crash_count, timestamp }) => {
+      platformFiltered.forEach(({ app_version, timestamp, ...rest }) => {
+        const crash_count = rest["crash.count"];
+
         timestamp.forEach((time, i) => {
           // Check if this timestamp already exists
           const existing = flattened.find((entry) => entry.timestamp === time);
